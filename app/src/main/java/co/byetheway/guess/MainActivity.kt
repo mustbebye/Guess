6100c532640c0e0dc386d0d9291a1b3810713c60
byetheway@me.com <byetheway@me.com>
2019-09-24 15:29:34 +0800

    initial commit

---
+++ app/src/main/java/co/byetheway/guess/MainActivity.kt
@@ -0,0 +1,13 @@
+package co.byetheway.guess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        R.color.messageColor
+    }
+}
