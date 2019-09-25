4f5d0bc5ba44585b54bb2792dd8cfdb5326acb29
byetheway@me.com <byetheway@me.com>
2019-09-25 13:03:31 +0800

    function design, toast, dialog

--- app/src/main/java/co/byetheway/guess/MainActivity.kt
+++ app/src/main/java/co/byetheway/guess/MainActivity.kt
@@ -2,12 +2,40 @@ package co.byetheway.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val secretNumber = SecretNumber()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        R.color.messageColor
+        Log.d("MainActivity", "secret: ${secretNumber.secret}")
+    }
+
+    fun check(view: View) {
+
+        val number = ed_number.text.toString().toInt()
+        val diff = secretNumber.validate(number)
+        var message = "Yes, you got it!"
+
+        if (diff < 0) {
+            message = "Bigger"
+        } else if (diff > 0) {
+            message = "Smaller"
+        }
+
+//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(message)
+            .setPositiveButton("OK", null)
+            .show()
     }
 }
